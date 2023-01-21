ed0790deb9358d641175f580662b5aef55f53eff
Mikkeep <mikke.pennanen@gmail.com>
2023-01-21 09:07:51 +0200

    Schemas for validating the submitted input to the server

---
+++ server/app/schemas.py
@@ -0,0 +1,36 @@
+# More info on:
+# https://fastapi.tiangolo.com/tutorial/sql-databases/
+
+from pydantic import BaseModel
+
+
+class MessageBase(BaseModel):
+    message: str
+    api_token: str
+
+
+class MessageCreate(MessageBase):
+    pass
+
+
+class Message(MessageBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    username: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    api_token: str
+
+    class Config:
+        orm_mode = True
