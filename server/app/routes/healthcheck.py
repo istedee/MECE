54c56ef8b3cfacc114560a7d7f55e1d820bd3048
Mikkeep <mikke.pennanen@gmail.com>
2023-01-25 12:26:35 +0200

    Added healthcheck to router instead of being in the main

---
+++ server/app/routes/healthcheck.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+
+
+router = APIRouter(
+    tags=["messages"],
+    responses={403: {"description": "Invalid credentials"}},
+)
+
+
+@router.get(
+    "/ping",
+    tags=["health-check"],
+    responses={200: {"description": "Returns 'pong' as status check msg"}},
+)
+async def root():
+    return {"message": "pong"}
