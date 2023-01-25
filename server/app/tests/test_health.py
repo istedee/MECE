042375b855b656c81710ca4498554f7b2ad61fdf
Mikkeep <mikke.pennanen@gmail.com>
2023-01-25 15:11:38 +0200

    Changed testclient name

--- server/app/tests/test_health.py
+++ server/app/tests/test_health.py
@@ -4,8 +4,8 @@
 # https://fastapi.tiangolo.com/tutorial/testing/
 
 
-def test_health_check(test_app):
+def test_health_check(client):
     """Test that the health check of the server works"""
-    response = test_app.get("/ping")
+    response = client.get("/ping")
     assert response.status_code == 200
     assert response.json() == {"message": "pong"}
