abb5e898efb2ec114b34c76b5093aabc0c7e5a89
Mikkeep <mikke.pennanen@gmail.com>
2023-01-25 15:12:07 +0200

    Added tests and changed test client name

--- server/app/tests/test_users.py
+++ server/app/tests/test_users.py
@@ -1,11 +1,25 @@
 """Tests for users route"""
 
 
-def test_user_registration(test_app):
+def auth(client, username="Bob", password="Alice", url="/users/register/"):
+    """Helper function for auth"""
+    userinfo = {"username": username, "password": password}
+    response = client.post(url, json=userinfo)
+    return response
+
+
+def test_user_registration(client):
     """Test registrating an user"""
-    userinfo = {"username": "Bob", "password": "Alice"}
-    response = test_app.post("/users/register/", json=userinfo)
+    response = auth(client)
     assert response.status_code == 200
     resp = response.json()
     assert resp["username"] == "Bob"
-    assert "api_token" in resp.keys()
+    assert "api_token" in resp
+
+
+def test_apitoken_check(client):
+    """Test fetching api token for user"""
+    response = auth(client, url="/users/check-api-token/")
+    assert response.status_code == 200, response.text
+    resp = response.json()
+    assert "api_token" in resp
