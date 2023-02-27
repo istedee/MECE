2ddbd61ca88d55d7057a2e786cb350b9161b5789
Mikkeep <mikke.pennanen@gmail.com>
2023-02-27 16:24:10 +0200

    Send produce messages to the fastapi instance

--- client/producer.py
+++ client/producer.py
@@ -11,10 +11,10 @@ def serializer(message):
     return json.dumps(message).encode('utf-8')
 
 # Kafka Producer
-producer = KafkaProducer(
-    bootstrap_servers=['localhost:9092'],
-    value_serializer=serializer
-)
+# producer = KafkaProducer(
+#     bootstrap_servers=['localhost:9092'],
+#     value_serializer=serializer
+# )
 
 # if __name__ == '__main__':
     # Infinite loop - runs until you kill the program
