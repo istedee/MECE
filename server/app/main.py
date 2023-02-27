1eccb0aee201b12f951e84dc05727ed4187c8122
Mikkeep <mikke.pennanen@gmail.com>
2023-02-27 16:24:35 +0200

    Remove broker info from startup

--- server/app/main.py
+++ server/app/main.py
@@ -8,35 +8,9 @@ from fastapi import FastAPI
 import models
 from db import engine, SessionLocal
 
-from kafka import KafkaAdminClient
-from kafka.admin import NewTopic
-from kafka.errors import TopicAlreadyExistsError
-
 load_dotenv(verbose=True)
 
 app = FastAPI()
-
-@app.on_event("startup")
-async def startup_event():
-    #wait for Kafka broker to be ready
-    time.sleep(1)
-    # Kafka Admin Client
-    client = KafkaAdminClient(
-        bootstrap_servers=os.environ.get("BOOTSTRAP-SERVERS"))
-    # Creating topic
-    topic = NewTopic(name=os.environ.get("TOPIC_MESSAGES_BASIC_NAME"),
-                     num_partitions=int(os.environ.get("TOPIC_MESSAGES_BASIC_PARTITIONS")),
-                     replication_factor=int(os.environ.get("TOPIC_MESSAGES_BASIC_REPLICATION_FACTOR")))
-    # If topic already exists, it will throw an error
-    try:
-        # Creating topic
-        client.create_topics(new_topics=[topic], validate_only=False)
-    except TopicAlreadyExistsError:
-        print("Topics already set up")
-        pass
-    finally:
-        # Close the client
-        client.close()
 
 # Init db if not existing
 # Database connection function for use in routers
