882deb39ff06aa4310a77ccc1bf42d356245711e
ansantil <anssi.antila@hotmail.com>
2023-03-10 19:39:25 +0200

    default to sqlite

--- server/app/db.py
+++ server/app/db.py
@@ -1,18 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import exc
 
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./server.db"
-SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://anssi:toor@172.30.29.203:3306/sampledb"
-
-#engine = create_engine(
-#    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-#)
-
-engine = create_engine(
+try:
+    # if using mysql  database
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./server.db"
+    engine = create_engine(
     SQLALCHEMY_DATABASE_URL
-)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
+    )
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    Base = declarative_base()
+except exc.DatabaseError:
+    SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://root:toor@127.0.0.1/sampledb"
+    engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+    )
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    Base = declarative_base()
+    
