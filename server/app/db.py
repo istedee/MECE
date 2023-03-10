3a3deecaf7f31a1eda1073f9d38a5d7b72fa83a2
ansantil <anssi.antila@hotmail.com>
2023-03-10 19:44:09 +0200

    use env variable to set external db ip

--- server/app/db.py
+++ server/app/db.py
@@ -2,17 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import exc
+import os
 
-try:
-    # if using mysql  database
-    SQLALCHEMY_DATABASE_URL = "sqlite:///./server.db"
+
+if os.environ.get("EXTERNAL-IP"):
+    IP = os.environ.get("EXTERNAL-IP")
+    SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://root:toor@{IP}/sampledb"
     engine = create_engine(
     SQLALCHEMY_DATABASE_URL
     )
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     Base = declarative_base()
-except exc.DatabaseError:
-    SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://root:toor@127.0.0.1/sampledb"
+else:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./server.db"
     engine = create_engine(
     SQLALCHEMY_DATABASE_URL
     )
