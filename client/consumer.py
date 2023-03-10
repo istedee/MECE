42ee66e59420206e60e6865298772dd62fbb0d27
Mikkeep <mikke.pennanen@gmail.com>
2023-03-10 08:02:20 +0200

    consumer config, deprecated

--- client/consumer.py
+++ client/consumer.py
@@ -4,9 +4,15 @@ from kafka import KafkaConsumer
 if __name__ == '__main__':
     # Kafka Consumer 
     consumer = KafkaConsumer(
-        'messages',
-        bootstrap_servers='localhost:9092',
-        auto_offset_reset='earliest'
+        #bOPj3nrR
+        #M8mZs81j
+        #w0TnHx5p
+        'bOPj3nrR',
+        bootstrap_servers='kafka-route-chatexperience.rahtiapp.fi/',
+        # 10.131.6.19
+        #kafka-route-chatexperience.rahtiapp.fi
+        auto_offset_reset='earliest',
+        api_version=(0, 10, 2)
     )
     for message in consumer:
         print(json.loads(message.value))
